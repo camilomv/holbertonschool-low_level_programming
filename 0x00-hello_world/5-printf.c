f7cd4bb8ffd69eda1945d205eeca77cb5f4ef4d5
camilomv <montenegrocamilo@hotmail.com>
2020-06-11 16:49:14 +0000

    Fixing warning in Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-/* main is to learn how to use printf */
-/* Return = 0 */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
