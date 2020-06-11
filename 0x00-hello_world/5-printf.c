f82219720b9617a113265b3300c02ff5fcb99b51
camilomv <montenegrocamilo@hotmail.com>
2020-06-11 16:41:17 +0000

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/* main is to learn how to use printf */
+/* Return = 0 */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
