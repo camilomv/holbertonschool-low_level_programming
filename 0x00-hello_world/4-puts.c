da443d3b3197deec5dd5a9b006c8f02f74b00b59
camilomv <montenegrocamilo@hotmail.com>
2020-06-14 17:19:10 +0000

    Task number 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+/**
+ * main - simple program to write a sentence
+ * Return: Shoul be 0 if everything ok
+ */
+
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
