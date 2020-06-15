dc1dd55eee0b43cbd9e00dbe52783e5d31f0826a
camilomv <montenegrocamilo@hotmail.com>
2020-06-15 01:42:15 +0000

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This program prints the alphabet
+ * Return: 0 if everything ok
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c != 'z'; c++)
+		putchar(c);
+	putchar(c);
+	return (0);
+}
