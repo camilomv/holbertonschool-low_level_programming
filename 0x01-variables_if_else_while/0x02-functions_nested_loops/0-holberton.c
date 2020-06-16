97574384540ee86de27da36eeb4fb4b1fedaf626
camilomv <montenegrocamilo@hotmail.com>
2020-06-16 14:10:14 +0000

    Firts necessary files

---
+++ 0x01-variables_if_else_while/0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * main - this program prints the word Holberton
+ * Return: 0 if everything ok
+ */
+
+int main(void)
+{
+        char(c);
+
+        _putchar('H'); 
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('o');
+	_putchar('\n');
+	return (0);
+}
