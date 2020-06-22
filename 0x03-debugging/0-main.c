8d7030f76e9a04c0a16866ba61b6ab3bd3bbf99f
camilomv <montenegrocamilo@hotmail.com>
2020-06-22 03:14:33 +0000

    Uploading first files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - testing that the function positive_or_negative works with 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,6 @@
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdio.h>
+
+void positive_or_negative(int i);
