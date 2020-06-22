952215a7b20a8b8e753ee57a3c0aa4b950b0820e
camilomv <montenegrocamilo@hotmail.com>
2020-06-22 03:47:18 +0000

    Fix Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,11 @@ int main(void)
 
         i = 0;
 
-        while (i < 10)
-        {
-                putchar(i);  /** It is necessary to increment i */
-        }
-
+        /** while (i < 10)
+	 * {
+         * putchar(i);
+	 * }
+	 */
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
