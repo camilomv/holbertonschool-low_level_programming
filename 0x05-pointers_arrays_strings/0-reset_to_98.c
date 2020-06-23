26ef828ae926bf62cf5f63acb808556e77774bc4
camilomv <montenegrocamilo@hotmail.com>
2020-06-23 00:30:47 +0000

    Task 0 ready

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - takes a pointer to an int and updates the value to 98
+ * @n: this is the pointer that is changed by the function
+ * Return: 98 as a new value
+ */
+
+int reset_to_98(int *n)
+{
+	*n = 98;
+	return (*n);
+}
