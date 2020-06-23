bd09de03ff65b2803b1031600529369829d59118
camilomv <montenegrocamilo@hotmail.com>
2020-06-23 04:27:15 +0000

    Task 9 finally

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+
+/**
+ * times_table - function that prints the 9 times table
+ *
+ * Return: nothing (void)
+ */
+
+void times_table(void)
+{
+	int n;
+	int mult;
+	int rs;
+
+
+	for(n = 0; n <= 9; n++)
+	{
+		for(mult = 0; mult <= 9; mult++)
+		{
+			rs = n * mult;
+			if (mult == 0)
+			{
+				_putchar(rs % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if ((rs < 10) && (mult != 0) && (mult != 9))
+			{
+				_putchar(' ');
+				_putchar(rs % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if ((rs >= 10) && (mult != 9))
+			{
+				_putchar(rs / 10 + '0');
+				_putchar(rs % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (mult == 9)
+			{
+				if (rs > 9)
+				{
+					_putchar(rs / 10 + '0');
+					_putchar(rs % 10 + '0');
+				}
+				else if (rs <= 9)
+				{
+					_putchar(' ');
+					_putchar(rs % 10 + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
